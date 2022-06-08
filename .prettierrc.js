67a53cc2f70220e0b30374c5ed271918f5eb40b1
Alex Van Liew <alexv@productiv.com>
2022-06-07 21:45:51 -0700

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 120,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+};
